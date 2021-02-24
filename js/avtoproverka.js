a73fc5d3e4de4cc4cd8c9b8d539b0ec235c46bd7
Roman <4895476@gmail.com>
2021-02-24 23:12:30 +0200

    module 3 - задача 23

--- js/avtoproverka.js
+++ js/avtoproverka.js
@@ -68,3 +68,21 @@ let arr = [];
 return arr;
 }
 
+/// module 3 - задача 23, тупняк с введением нового ключа айкон
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// Пиши код ниже этой строки
+
+const {
+    yesterday: highYesterday,
+    today: highToday,
+    tomorrow: highTomorrow,
+    icon: highIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+} = highTemperatures;
+
+
+// Пиши код выше этой строки
+const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
